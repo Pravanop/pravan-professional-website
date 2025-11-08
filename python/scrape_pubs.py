3d0e7992af5f32048923919362a088dea85c244c
Pravan Omprakash <pravanomprakash.181mt035@nitk.edu.in>
2025-11-07 20:53:21 -0600

    adding images

--- python/scrape_pubs.py
+++ python/scrape_pubs.py
@@ -22,7 +22,7 @@ def fetch_by_id(author_id, max_pubs=50):
     return pubs
 
 if __name__ == "__main__":
-    author_id = "MzkdGtsAAAAJ&hl=en"  # ← replace with your ID
+    author_id = "MzkdGtsAAAAJ&hl"  # ← replace with your ID
     pubs = fetch_by_id(author_id)
     with open("publications.json", "w", encoding="utf-8") as f:
         json.dump(pubs, f, indent=2, ensure_ascii=False)
